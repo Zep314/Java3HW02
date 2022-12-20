fa8cee45aedd09f8bbd3b44b80b24f5cdf8a69f0
Dmitry Mordanov <mordanov314@gmail.com>
2022-12-20 10:36:19 +0300

    initial commit

---
+++ task02/src/Main.java
@@ -0,0 +1,16 @@
+/*
+Задание:
+try {
+   int d = 0;
+   double catchedRes1 = intArray[8] / d;
+   System.out.println("catchedRes1 = " + catchedRes1);
+} catch (ArithmeticException e) {
+   System.out.println("Catching exception: " + e);
+}
+*/
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
